4ea6568bd85789680acdcb93189da2ea0642a696
tommhe14 <53539004+tommhe14@users.noreply.github.com>
2025-02-01 22:11:56 +0000

    self api update
    added pre checked default headers for users who are requesting on a deployment

--- sofascore_wrapper/api.py
+++ sofascore_wrapper/api.py
@@ -3,12 +3,15 @@ import aiohttp
 BASE_URL = "https://www.sofascore.com/api/v1"
 
 class SofascoreAPI:
-    def __init__(self):
+    def __init__(self, headers: dict = None):
+        default_headers = {"User-Agent": "Mozilla/5.0"}
         self.session = None
+        self.headers = default_headers or headers
+
 
     async def _get(self, endpoint):
         if self.session is None:
-            self.session = aiohttp.ClientSession()
+            self.session = aiohttp.ClientSession(headers = self.headers)
         async with self.session.get(f"{BASE_URL}{endpoint}") as response:
             if response.status == 200:
                 return await response.json()
