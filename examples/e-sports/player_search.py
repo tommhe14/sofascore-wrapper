3c67cbe856c42a8a858b6cd7a68d67166137e655
tommhe14 <53539004+tommhe14@users.noreply.github.com>
2025-02-02 18:59:07 +0000

    mane

--- examples/e-sports/player_search.py
+++ examples/e-sports/player_search.py
@@ -10,15 +10,11 @@ async def player_search():
 
     init_player = Search(api, search_string = "lewis hamilton")
     search_player = await init_player.search_all(sport = "motorsport")
-    #print(json.dumps(search_player, indent = 4))
-
-    init_match = Match(api)
-    match = await init_match.games_by_date(sport="football")
-    print(json.dumps(match, indent = 4))
+    print(json.dumps(search_player, indent = 4))
 
     init_dedicated_search = PlayerSearch(api, query = "s1mple")
     search = await init_dedicated_search.search_player()
-    #print(json.dumps(search, indent = 4))
+    print(json.dumps(search, indent = 4))
 
     await api.close()
 asyncio.run(player_search())
