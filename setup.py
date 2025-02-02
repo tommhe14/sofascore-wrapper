d3240b1c71bcb8e2b624f814feb5cc0a856e9a8e
tommhe14 <53539004+tommhe14@users.noreply.github.com>
2025-02-02 18:38:24 +0000

    mane

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="sofascore_wrapper",  
-    version="1.0.10",
+    version="1.0.11",
     description="A Python API wrapper for https://www.sofascore.com",
     long_description=README,
     long_description_content_type="text/markdown",
