c63a215af5d2dfea5ee9723b9bd8ac25338a3ef0
tommhe14 <53539004+tommhe14@users.noreply.github.com>
2025-06-29 00:29:33 +0100

    mane

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="sofascore_wrapper",
-    version="1.1.0",
+    version="1.1.1",
     description="Python API wrapper for SofaScore",
     long_description=README,
     long_description_content_type="text/markdown",
